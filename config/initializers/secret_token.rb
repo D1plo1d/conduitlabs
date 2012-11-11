d8ca41ade7edf390d64d811607a23113ea57e10f
Justin Bull <me@justinbull.ca>
2012-11-10 19:47:02 -0500

    Added skeleton Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conduitlabs::Application.config.secret_token = '20a072584870189417d648bc9b79aa01888b9e4d11619f54527898a788b8354debac5dd8a33d53de7ffe8a2ef069c80b73841ba99e7a05b488408f1b93322007'
